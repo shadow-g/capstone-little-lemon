93e308ba60506950d2ae734d411bcddc275c7406
Keyna Kloever <keyna.kloever@gmail.com>
2023-10-29 17:45:51 +0100

    "Applied changes to stock images"

--- src/components/sections/headingPages/Heading.js
+++ src/components/sections/headingPages/Heading.js
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 export default function Heading() {
     return (
         <header>
@@ -7,15 +7,15 @@ export default function Heading() {
                     <h1>Little Lemon</h1>
                     <h2>Chicago</h2>
                     <p className="subsection">We are a family owned Mediterranean restaurant, located on  Maldove Street in Chicago, Illionis. We focus
-                    on traditional recipes served with a modern twist.</p>
+                        on traditional recipes served with a modern twist.</p>
                     <br></br>
                     <Link className="action-button" to="/reservations">Reserve a table</Link>
                 </section>
 
                 <section className="hero-image">
-                    <img src={require('../../../assets/food/food1.png')} alt="Little Lemon restaurant cuisine"></img>
+                    <img src={require('../../../assets/restauranfood.jpg')} alt="Little Lemon restaurant cuisine"></img>
                 </section>
             </article>
-      </header>
+        </header>
     );
 }
